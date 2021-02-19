205c243b7679f1f0b30439a31c8a35307095f325
ohahaha29 <ohahaha29@gmail.com>
2021-02-19 11:20:18 +0900

    파이썬 코멘트, 변수

--- programming_in_python/hello.py
+++ programming_in_python/hello.py
@@ -1,1 +1,15 @@
+# 코멘트(주석): 복잡한 코드 설명, 하다가 만 부분 표시, 다른 개발자들과 소통
 print("Hello world!")
+
+# 변수
+kitkat = 190
+oreos = 502
+pringles = 292
+twix = 135.9
+cheetos = 485
+
+# 다양한 과자 조합
+print(kitkat + oreos * 2)
+print(cheetos * 4)
+print(pringles + oreos + twix)
+print(pringles * 3 + oreos * 2)
